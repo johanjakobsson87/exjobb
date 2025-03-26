e4c7e9b1743de344afc3afd53d1eaaa2c3cab601
johanjakobsson87 <112547106+johanjakobsson87@users.noreply.github.com>
2025-03-26 10:50:15 +0100

    Första pushen
    Har skrivit en generator för att skapa Jitter-filer och en main som suger igång allt

---
+++ Kod/JitterGenerator.py
@@ -0,0 +1,34 @@
+# Program that takes JitterStart and JitterLength as inputs and generates a jitterfile (.txt) as output.
+
+class JitterFileGenerator:
+    def __init__(self, numberOfFrames = 275, startingMS = 520):
+        self.numberOfFrames = numberOfFrames
+        self.startingMS = startingMS
+
+    def generate_jitter_file(self, JitterStart, JitterLength):
+        timestamps = []
+        
+        # Create array of timestamps
+        for i in range(0, self.numberOfFrames):
+            MS = self.startingMS + 20*i    
+            timestamps.append(MS)
+        
+        # Get start and end index of jitter
+        jitterStartIndex = int((JitterStart - self.startingMS) / 20 )
+        print("Jitter start index: ", jitterStartIndex)
+        jitterEndIndex =  int(((JitterStart + JitterLength)- self.startingMS) / 20)-1
+        print("Jitter end index: ", jitterEndIndex)
+
+        # Add jitter between the startindex and the endindex of the jitter. All following timestamps increments by 20
+        for i in range(jitterStartIndex, jitterEndIndex+1):
+            if i < self.numberOfFrames:
+                timestamps[i] = JitterStart                
+        for i in range(jitterEndIndex+1, self.numberOfFrames):
+            timestamps[i] = timestamps[i-1] + 20
+
+        # Create JitterFile.txt
+        with open(f"./jitterfiles/jitterfile_Burstjitter_{JitterLength}msDelayAt{JitterStart}.txt", "w") as f:
+            for i in range(self.numberOfFrames):
+                f.write(f"{i+1}, {timestamps[i]}\n")
+                
+                
