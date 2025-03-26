edccc73ca65c291ed311ee33b4e71a3eeb99419a
johanjakobsson87 <112547106+johanjakobsson87@users.noreply.github.com>
2025-03-26 13:16:16 +0100

    Fixat jittergrejs

--- Kod/JitterGenerator.py
+++ Kod/JitterGenerator.py
@@ -1,5 +1,11 @@
 # Program that takes JitterStart and JitterLength as inputs and generates a jitterfile (.txt) as output.
 
+
+
+
+
+
+
 class JitterFileGenerator:
     def __init__(self, numberOfFrames = 275, startingMS = 520):
         self.numberOfFrames = numberOfFrames
@@ -16,13 +22,13 @@ class JitterFileGenerator:
         # Get start and end index of jitter
         jitterStartIndex = int((JitterStart - self.startingMS) / 20 )
         print("Jitter start index: ", jitterStartIndex)
-        jitterEndIndex =  int(((JitterStart + JitterLength)- self.startingMS) / 20)-1
+        jitterEndIndex =  int(((JitterStart + JitterLength)- self.startingMS) / 20)
         print("Jitter end index: ", jitterEndIndex)
 
         # Add jitter between the startindex and the endindex of the jitter. All following timestamps increments by 20
-        for i in range(jitterStartIndex, jitterEndIndex+1):
+        for i in range(jitterStartIndex, jitterEndIndex):
             if i < self.numberOfFrames:
-                timestamps[i] = JitterStart                
+                timestamps[i] = JitterStart + JitterLength                
         for i in range(jitterEndIndex+1, self.numberOfFrames):
             timestamps[i] = timestamps[i-1] + 20
 
@@ -31,4 +37,14 @@ class JitterFileGenerator:
             for i in range(self.numberOfFrames):
                 f.write(f"{i+1}, {timestamps[i]}\n")
                 
-                
+
+
+def main():
+    
+    
+    JFgenerator = JitterFileGenerator()
+    JFgenerator.generate_jitter_file(1540, 1500)
+
+
+if __name__ == "__main__":
+    main()
