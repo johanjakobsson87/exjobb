8f4bad429e475f9096dbacdc8a4367dd2f675ca7
johanjakobsson87 <112547106+johanjakobsson87@users.noreply.github.com>
2025-04-17 08:27:49 +0200

    Minor edits

--- Kod/logconverter/logconverter.py
+++ Kod/logconverter/logconverter.py
@@ -1,13 +1,14 @@
-# A program that iterates through the .txt-files in a folder, open the file and exchanges all commas to tabulations.
+# A script that iterates through the .txt-files in a folder, open the file and exchanges all commas to tabulations.
 # Put logs from subjective tests in ./logs folder, then execute this program by running python logconverter.py in the terminal. Logs will now be tabulated and will be easier to enter into spreadsheet.
+
 import os
 
 pathToUnprocessedLogs = "./logs"
 pathToConvertedLogs = "./convertedlogs"
 fileEnding = ".txt"
+alltexts = ""
 
 os.makedirs(pathToConvertedLogs, exist_ok=True)
-alltexts = ""
 
 for filename in os.listdir(pathToUnprocessedLogs):
     if filename.endswith(fileEnding):
@@ -21,6 +22,5 @@ for filename in os.listdir(pathToUnprocessedLogs):
             text = "\n" + f"User: {user}, Subjective test: {test}"  + "\n" + text
             text=text.replace(",", "\t")
             alltexts += text
-
 with open(os.path.join(pathToConvertedLogs, "converted_logs.txt"), "w") as f:
     f.write(alltexts)
---
+++ Kod/logconverter/run_logconverter.bat
@@ -0,0 +1,1 @@
+call python logconverter.py
